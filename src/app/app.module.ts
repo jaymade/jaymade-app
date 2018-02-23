f3aa2135181f62c20c940083f2a0ccb612d6263e
JayMade <jay@jaymade.com>
2018-02-23 11:35:40 -0500

    add angular bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,23 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from './../environments/environment';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
